98bd849612592f8115890ed09a3585aaaa870243
vademecum0738 <nifyzy@gmail.com>
2026-04-28 18:48:29 -0400

    Homework 1 part!!!!

---
+++ src/Homework2.java
@@ -0,0 +1,43 @@
+import javax.lang.model.element.NestingKind;
+
+public class Homework2 {
+    private String size;
+    private String age;
+    private String temperature;
+    private String distance;
+    public Homework2(String size, String age, String temperature, String distance){
+        this.size=size;
+        this.age=age;
+        this.temperature=temperature;
+        this.distance=distance;
+    }
+    public String getSize(){
+        return size;
+    }
+    public void setSize(String size){
+        this.size=size;
+    }
+    public String getAge(){
+        return age;
+    }
+    public void setAge(String age){
+        this.age=age;
+    }
+    public String getTemperature(){
+        return temperature;
+    }
+    public void setTemperature(String temperature){
+        this.temperature=temperature;
+    }
+    public String getDistance(){
+        return distance;
+    }
+    public void setDistance(String distance){
+        this.distance=distance;
+    }
+
+    @Override
+    public String toString() {
+        return"size: "+size+"\nage: "+age+"\ntemperature: "+temperature+"\ndistance: "+distance;
+    }
+}
---
+++ src/Homework2main.java
@@ -0,0 +1,6 @@
+public class Homework2main {
+    public static void main(String[] args) {
+        Homework2 homework2=new Homework2("1.4 million km wide","4.6 billion year old","5.500 C","150 million km from earth");
+        System.out.println(homework2);
+    }
+}
